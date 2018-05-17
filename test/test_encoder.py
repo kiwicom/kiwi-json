0d87865ca284ccbf344293cebd3cc37637125ce3
yedpodtrzitko <yed@vanyli.net>
2018-05-17 17:09:15 +0200

    parametrize tests

--- test/test_encoder.py
+++ test/test_encoder.py
@@ -1,10 +1,44 @@
 import datetime
 from decimal import Decimal
 import enum
+import uuid
 
 import pytest
+from pytz import UTC
 
 from kw.json import default_encoder
+
+
+class Custom:
+    pass
+
+
+class HTML:
+    def __html__(self):
+        return 'foo'
+
+
+class SomeEnum(enum.Enum):
+    red = 1
+    green = 2
+
+
+UUID = uuid.uuid4()
+
+
+@pytest.mark.parametrize('value, expected', (
+        ({1}, [1]),
+        (Decimal('1'), '1'),
+        (UUID, str(UUID)),
+        (datetime.datetime(2018, 1, 1), '2018-01-01T00:00:00'),
+        (datetime.datetime(2018, 1, 1, tzinfo=UTC), '2018-01-01T00:00:00+00:00'),
+        (datetime.date(2018, 1, 1), '2018-01-01'),
+        (HTML(), 'foo'),
+        (SomeEnum.red, 'red'),
+        ({'foo': 1}.items(), {'foo': 1}),
+))
+def test_daisy_encoder(value, expected):
+    assert default_encoder(value) == expected
 
 
 def test_unknown_raises():
@@ -13,30 +47,3 @@ def test_unknown_raises():
 
     with pytest.raises(TypeError):
         default_encoder(Foo())
-
-
-def test_date():
-    dt = datetime.date(2018, 1, 2)
-
-    assert default_encoder(dt) == '2018-01-02'
-
-
-def test_datetime():
-    dt = datetime.datetime(2018, 1, 2, 3, 4, 5)
-
-    assert default_encoder(dt) == '2018-01-02T03:04:05'
-
-
-def test_decimal():
-    dec = Decimal(100 * 1.19)
-
-    assert default_encoder(dec) == '119'
-
-
-def test_enum():
-    class Cats(enum.Enum):
-        foo = 1
-        bar = 2
-
-    cat = Cats.foo
-    assert default_encoder(cat) == 'foo'
