de98cf84d50bd219809153fdec43077e0c8bb8a8
dmitry.dygalo <dmitry.dygalo@kiwi.com>
2018-06-27 14:56:31 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("test-requirements.txt") as f:
 
 setup(
     name="kiwi-json",
-    version="0.1.0",
+    version="0.2.0",
     url="https://gitlab.skypicker.com/finance/kiwi-json",
     author="yed podtrzitko",
     author_email="yed@kiwi.com",
