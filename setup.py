828145c0cb758dfe33bb839578cc226de10cce01
Baptiste Darthenay <baptiste.darthenay@kiwi.com>
2020-08-10 08:34:43 +0200

    Release 0.9.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("test-requirements.txt") as f:
 
 setup(
     name="kiwi-json",
-    version="0.8.0",
+    version="0.9.0",
     url="https://gitlab.skypicker.com/finance/kiwi-json",
     author="yed podtrzitko",
     author_email="yed@kiwi.com",
