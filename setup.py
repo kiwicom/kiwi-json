3a623ed07fcc72dc4d09686d4689b0192f8c9ec2
Lukas Vojt <lukas.vojt@kiwi.com>
2022-08-17 15:02:54 +0200

    feat: add python 3.9 and 3.10 support

--- setup.py
+++ setup.py
@@ -22,5 +22,7 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
 )
